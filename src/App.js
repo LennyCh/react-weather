c76e053b568fc47a9f06294dda33810ba4f6dd45
LennyCh <lena.chepkasova.95@mail.ru>
2020-08-11 17:29:29 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = '30cb993ec42d09e401df734028fff830';
+
+class App extends React.Component {
+
+    state = {
+        city: null,
+        temp: null,
+        icon: null,
+        error: null
+    };
+
+    gettingWeather = async (event) => {
+        event.preventDefault();
+        const city = event.target.elements.city.value;
+
+        if (city) {
+            const apiUrl = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+            const data = await apiUrl.json();
+
+            const tempC = Math.floor(data.main.temp - 273);
+
+            const iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+            this.setState({
+                city: data.name,
+                temp: tempC,
+                icon: iconUrl,
+            })
+        } else {
+            this.setState({
+                error: 'Dashboard is empty'
+            })
+        }
+    };
+
+  render() {
+    return (
+        <div>
+            <Info />
+            <Form weatherMethod={this.gettingWeather}/>
+            <Weather
+                city={this.state.city}
+                temp={this.state.temp}
+                icon={this.state.icon}
+                error={this.state.error}
+            />
+        </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = () => (
+    <div>
+        <h2>Enter the name of the city</h2>
+        <h3>City name can only contain Latin letters and numbers</h3>
+    </div>
+)
+
+export default Info;
